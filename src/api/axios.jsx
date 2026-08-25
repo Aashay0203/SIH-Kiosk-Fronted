6bbfa7765e9c0aaae2de5e86fa09c224cbebace9
aashay_02 <aashaygupta0203@gamil.com>
2026-08-25 12:50:42 +0530

    this is backend url fix

--- src/api/axios.jsx
+++ src/api/axios.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8080/api/",
-  /* "https://sih-kiosk-backend.onrender.com/api/" /* || import.meta.env.VITE_API_URL */ withCredentials: true,
+  baseURL: " https://sih-kiosk-backend.onrender.com/api/",
+  /* "http://localhost:8080/api/" /* || import.meta.env.VITE_API_URL */ withCredentials: true,
 });
 
 export default instance;
